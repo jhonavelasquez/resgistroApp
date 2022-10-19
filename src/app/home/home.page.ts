3cd84a839654afd6f94822664e816aff18b2cd3a
jhonavelasquez <jon.velasquez@duocuc.cl>
2022-10-18 21:51:07 -0300

    se agrego base de datos SQlite y login funcional

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
 import { ToastController } from '@ionic/angular';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
 
 @Component({
   selector: 'app-home',
@@ -12,32 +13,55 @@ export class HomePage {
   usuario: string;
   clave: string;
   hide = true;
+  private db: SQLiteObject;
+  constructor(
+    private toast: ToastController,
+    private router: Router,
+    private sqlite: SQLite
+  ) {
+    this.sqlite.create({
+      name: 'data.db',
+      location: 'default'
+    })
+      .then((db: SQLiteObject) => {
+        this.db = db;
+        db.executeSql('create table alumno(nombre VARCHAR(32), clave VARCHAR(32))', [])
+          .then(() => console.log('Tabla alumno creada'))
+          .catch(e => console.log(e));
+        //registro usuaio
+        this.db.executeSql('INSERT INTO alumno VALUES(?,?)', ['alumno','123456']).then(() => {
+          console.log("Insert ejecutado!")
+        }).catch(e => console.log(e));  
+    
+    
+      })
+      .catch(e => console.log(e));
+    
   
-  constructor(private toast:ToastController,
-    private router: Router) {}
-
-  limpiar(){
-    this.usuario="";
-    this.clave="";
   }
 
-  ingresar(){
-    let nav:NavigationExtras = {
-      state: {usuario: this.usuario}
+  limpiar() {
+    this.usuario = "";
+    this.clave = "";
+  }
+
+  ingresar() {
+    let nav: NavigationExtras = {
+      state: { usuario: this.usuario }
     }
-    if(this.usuario == "alumno" && this.clave == "12345678"){
+    if (this.usuario == "alumno" && this.clave == "12345678") {
       this.router.navigate(['/inicio-alumno'], nav)
     }
-    else if(this.usuario == "profesor" && this.clave == "12345678"){
+    else if (this.usuario == "profesor" && this.clave == "12345678") {
       this.router.navigate(['/inicio-profesor'], nav)
-      
-    }else{
+
+    } else {
       this.mostrarAlerta("Error, usuario o contraseña incorrecto(s)", 4000)
-    
+
     }
   }
 
-  async mostrarAlerta(mensaje:string, duracion:number){
+  async mostrarAlerta(mensaje: string, duracion: number) {
     const toast = await this.toast.create({
       message: mensaje,
       duration: duracion
@@ -45,4 +69,41 @@ export class HomePage {
     toast.present();
   }
 
+  //sqlite 
+  insertar() {
+    this.db.executeSql('INSERT INTO usuario VALUES(?)', ['alumno']).then(() => {
+      console.log("Insert ejecutado!")
+    }).catch(e => console.log(e));
+  }
+
+  select() {
+    this.db.executeSql('SELECT * FROM alumno ', []).then((data) => {
+      for (let i = 0; i < data.rows.length; i++) {
+        let item = data.rows.item(i).nombre;
+        console.log(item)
+      }
+    })
+  }
+
+  validarLogin(){
+    let nav: NavigationExtras = {
+      state: { usuario: this.usuario }
+    }
+    
+    this.db.executeSql('SELECT * FROM alumno WHERE ? = nombre and ? = clave', [this.usuario, this.clave]).then((data) => {
+      for (let i = 0; i < data.rows.length; i++) {
+        let item = data.rows.item(i).nombre;
+        console.log("funco")
+        this.router.navigate(['/inicio-alumno'], nav)
+      }
+    })
+    this.db.executeSql('SELECT * FROM alumno WHERE ? != nombre or ? != clave', [this.usuario, this.clave]).then((data) => {
+      for (let i = 0; i < data.rows.length; i++) {
+        let item = data.rows.item(i).nombre;
+        console.log("no funco")
+        this.mostrarAlerta("Error, usuario o contraseña incorrecto(s)", 3000)
+      }
+    })
+  }
+  
 }
